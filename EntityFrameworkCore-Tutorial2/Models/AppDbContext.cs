1321208590cf5a0767c63365a672948a79ec5f5a
Jeff Workman <jworkhollr@aol.com>
2022-02-13 13:12:53 -0500

    Add project files.

---
+++ EntityFrameworkCore-Tutorial2/Models/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore_Tutorial2.Models {
+
+    public class AppDbContext : DbContext  {
+            // from 2022-02-11 file 20220211 AM1 EFramwork audio file apple
+                        //type         Property
+        public virtual DbSet<BandCustomer> BandCustomers { get; set; }
+        
+       
+
+        // Constructors
+        public AppDbContext() { } //default constructor
+        //    have to call the Parent Constuctor ..has parameter
+        //                  |-----Type-------------------|
+        //                  |generic Class--<need Angles>| |var name/ Call Parant constructor| 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        //----------------------Method-------overriding when Configuring is called instead
+        // -----------------------------of using the other Context b/c of inheriting
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            if(!builder.IsConfigured) {
+                builder.UseSqlServer("server=localhost\\sqlexpress;database=SalesDb1;trusted_connection=true");
+            }
+
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder) {
+        //    builder.Entity<item>(
+        //        e => e.HasIndex(x => x.Code)
+        //                .IsUnique(true)
+        //    );
+        //}
+
+    }
+}
---
+++ EntityFrameworkCore-Tutorial2/Program.cs
@@ -0,0 +1,27 @@
+﻿using EntityFrameworkCore_Tutorial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkCore_Tutorial2 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            //creating an instance of the class we created
+            AppDbContext context = new AppDbContext();
+
+            //var bandcustomer = context.BandCustomers.Find(1);
+            //Console.WriteLine($"{bandcustomer.Name} {bandcustomer.Sales:c}");
+
+            List<BandCustomer> bandCustomers = context.BandCustomers.ToList();
+            foreach (var bandCustomer in bandCustomers) {
+                Console.WriteLine($"{bandCustomer.Name,-20} {bandCustomer.Sales,10:c}");
+            }
+         
+        }
+    
+    }       
+    
+}
